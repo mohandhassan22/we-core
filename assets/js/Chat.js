e418ae87dccba2b5f7d3756ef57e82de8e961bdb
mohandhassan22 <mohandhassan2292004@gmail.com>
2026-08-06 23:53:22 +0300

    Add Chat.js for embedding chat widget

---
+++ assets/js/Chat.js
@@ -0,0 +1,7 @@
+<script
+  src="https://iygwhapcpdmsasqlfelv.supabase.co/storage/v1/object/public/widget/widget.js"
+  data-function-url="https://iygwhapcpdmsasqlfelv.supabase.co/functions/v1/we-core-ai"
+  data-anon-key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5Z3doYXBjcGRtc2FzcWxmZWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNDk5MDQsImV4cCI6MjA4NjkyNTkwNH0.jqU1fEc9kBkXcCfazH6aTnS2XWWzPv0bbixHZgjtrnQ"
+  data-project-ref="iygwhapcpdmsasqlfelv"
+  defer>
+</script>
